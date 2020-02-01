24c2701b32aac44e7c8782ba46d5f400e527a2a7
Ubuntu <ubuntu@ip-172-31-91-178.ec2.internal>
2020-02-01 12:56:42 +0000

    Created base.html and added basic code & linked gyms.html to base.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'working_it_out'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://root:adminpass@myfirstcluster-ikccs.mongodb.net/working_it_out?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_gyms')
+def get_gyms():
+    return render_template("gyms.html", gyms=mongo.db.gyms.find())
 
 
 if __name__ == '__main__':
