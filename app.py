6fc8afab65eeee31709db0dd92198a435a315e7e
Ubuntu <ubuntu@ip-172-31-91-178.ec2.internal>
2020-02-01 15:04:38 +0000

    Added html to landing page and created add_gym.html and edit_gym.html and created subsequent route in app.py

--- app.py
+++ app.py
@@ -13,7 +13,34 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_gyms')
 def get_gyms():
-    return render_template("gyms.html", gyms=mongo.db.gyms.find())
+    return render_template("home.html", gyms=mongo.db.gyms.find())
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+    
+# ADD_RIVER page:
+
+@app.route("/add_new_gym")
+def add_new_gym():
+    return render_template("add_gym.html")
+
+# This function POSTs the input data from the user, to the database:
+
+@app.route("/insert_gym", methods=['POST'])
+def insert_gym():
+    river = mongo.db.gyms
+    river.insert_one(request.form.to_dict())
+    return redirect(url_for('get_gym_names'))
+
+
+# Allows user to update information about a gym
+
+@app.route("/edit_gym/<gym_id>")
+def edit_gym(gym_id):
+    the_gym = mongo.db.gyms.find_one({"_id": ObjectId(gym_id)})
+    return render_template("update_gym.html", gym=the_gym)
+
 
 
 if __name__ == '__main__':
