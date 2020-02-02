b31aa0501ebb472386f4141de6dd0060d1d0062e
Ubuntu <ubuntu@ip-172-31-91-178.ec2.internal>
2020-02-02 23:20:41 +0000

    Attempted bug fix with flask_pymongo

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'working_it_out'
